d1b46102a1260d33342639f0c0fdc2a05e9bb301
Edge, Damon <warrenonedge@gmail.com>
2019-11-14 22:26:13 -0500

    Initial Commit

---
+++ src/main/java/no/mad/recpatcha/recaptchasecurityhub/RecaptchaSecurityHubApplication.java
@@ -0,0 +1,13 @@
+package no.mad.recpatcha.recaptchasecurityhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecaptchaSecurityHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecaptchaSecurityHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/mad/recpatcha/recaptchasecurityhub/RecaptchaSecurityHubApplicationTests.java
@@ -0,0 +1,13 @@
+package no.mad.recpatcha.recaptchasecurityhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecaptchaSecurityHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
