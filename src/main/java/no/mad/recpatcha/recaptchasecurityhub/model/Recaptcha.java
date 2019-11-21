c570f80cdf12249139fa9cc839c271d92d26d091
Edge, Damon <warrenondge@gmail.com>
2019-11-21 17:50:36 -0500

    adding validation for threshold to between 0 and 1

--- src/main/java/no/mad/recpatcha/recaptchasecurityhub/model/Recaptcha.java
+++ src/main/java/no/mad/recpatcha/recaptchasecurityhub/model/Recaptcha.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
@@ -28,5 +30,7 @@ public class Recaptcha {
     private String secretKey;
 
     @NotBlank(message = "Threshold must not be blank")
+    @DecimalMin(value = "0.0", message = "Threshold must be a Decimal value between 0.0 - 1.0")
+    @DecimalMax(value = "1.0", message = "Threshold must be a Decimal value between 0.0 - 1.0")
     private Double threshold;
 }
